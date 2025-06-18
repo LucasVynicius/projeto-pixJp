ec9577e76114519b2621e291b09bd7636ad07fab
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2025-06-18 07:20:11 -0300

    Feat: Criar BilheteServiceImpl

--- src/main/java/br/com/lucas/pixjp_backend/service/BilheteService.java
+++ src/main/java/br/com/lucas/pixjp_backend/service/BilheteService.java
@@ -1,9 +1,23 @@
 package br.com.lucas.pixjp_backend.service;
 
+import br.com.lucas.pixjp_backend.dtos.BilheteCriadoResponse;
+import br.com.lucas.pixjp_backend.dtos.CriarBilheteRequest;
+import br.com.lucas.pixjp_backend.dtos.CriarUsuarioRequest;
+import br.com.lucas.pixjp_backend.model.Bilhete;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface BilheteService {
 
+    BilheteCriadoResponse criarBilhete(CriarBilheteRequest criarBilheteRequest);
 
+    Bilhete buscarBilhetePeloId(Long id);
+
+    List<Bilhete> buscarBilhetes ();
+
+    Bilhete atualizarBilhete(Long id, CriarBilheteRequest criarBilheteRequest);
+
+    void deletarBilhetePeloId(Long id);
 }
--- src/main/java/br/com/lucas/pixjp_backend/service/BilheteServiceImpl.java
+++ src/main/java/br/com/lucas/pixjp_backend/service/BilheteServiceImpl.java
@@ -1,8 +1,13 @@
 package br.com.lucas.pixjp_backend.service;
 
+import br.com.lucas.pixjp_backend.dtos.BilheteCriadoResponse;
+import br.com.lucas.pixjp_backend.dtos.CriarBilheteRequest;
+import br.com.lucas.pixjp_backend.model.Bilhete;
 import br.com.lucas.pixjp_backend.repositories.BilheteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +16,30 @@ public class BilheteServiceImpl implements BilheteService {
     private final BilheteRepository bilheteRepository;
 
 
+    @Override
+    public BilheteCriadoResponse criarBilhete(CriarBilheteRequest criarBilheteRequest) {
+        Bilhete bilhete = new Bilhete();
+
+        bilhete.setNumero(criarBilheteRequest.numero());
+    }
+
+    @Override
+    public Bilhete buscarBilhetePeloId(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Bilhete> buscarBilhetes() {
+        return List.of();
+    }
+
+    @Override
+    public Bilhete atualizarBilhete(Long id, CriarBilheteRequest criarBilheteRequest) {
+        return null;
+    }
+
+    @Override
+    public void deletarBilhetePeloId(Long id) {
+
+    }
 }
